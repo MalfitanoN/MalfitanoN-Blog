d4f76ba4cfdba2b0fe587b34b4bc66baeb0deef4
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2015-01-30 15:02:19 -0800

    New Code with comments

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,14 +1,31 @@
 <?php
+//This links this page to database.php 
+require_once (__DIR__ . "/../model/database.php");
 
-require_once ("../model/database.php");
-
+//this connnects the host username and password 
 $connection = new mysqli($host, $username, $password);
 
+//if there is an error this message will pop up on screen and 
+//it will kill the code so it doesnt run
 if ($connection->connect_error) {
     die("Error: " . $connection->connect_error);
 }
- else {
-    echo 'Success: ' . $connection->host_info;    
+
+//Linking the database
+$exists = $connection->select_db($database);
+
+//to check if the database exists
+if(!$exists){
+    $query = $connection->query("CREATE DATABASE $database");
+    
+    if($query){
+        echo "YEA DATABASE CREATED: " . $database;
+    }
 }
 
+ else {
+        echo "DATA BASE ALREADY EXISTS";
+    }
+
+//closing the connection
 $connection->close();
--- model/database.php
+++ model/database.php
@@ -1,6 +1,13 @@
 <?php
 
+    //Name of host
     $host = "localhost";
+    
+    //username which is root
     $username = "root";
+    
+    //Password which is root
     $password = "root";
+    
+    //Data base name
     $database  = "blog_db";
