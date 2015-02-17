053a2902ef50bd3aef272d89720a44d613f6b4ed
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2015-02-17 15:05:47 -0800

    39

--- controller/create-db.php
+++ controller/create-db.php
@@ -10,12 +10,28 @@ require_once (__DIR__ . "/../model/config.php");
          . "post text NOT NULL,"
          . "PRIMARY KEY (id))");
  
- //When the table posts is first created
-    if($query){
+//When the table posts is first created
+  if($query){
         echo " Sucessfully Created Table: Posts";
     }
-    //this tells you the what its connected to which is the Table Posts
+//this tells you the what its connected to which is the Table Posts
   else {
        echo  "<p>" . $_SESSION["connection"]->error . "</p>";
   }
 
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+//When the table posts is first created
+  if($query){
+        echo " Sucessfully Created Table: users";
+    }
+//this tells you the what its connected to which is the Table Posts
+  else {
+       echo  "<p>" . $_SESSION["connection"]->error . "</p>";
+  }
